8fa0ea9b4ffe3fbf6518142fe92c743c44ce6b64
Luís Ângelo Rodrigues Júnior <luisangelorjr@gmail.com>
2016-10-10 20:31:20 -0300

    Update GitHub-Verao_v5.bat
    %ip% mudado  para  %IPv4%

--- GitHub-Verao_v5.bat
+++ GitHub-Verao_v5.bat
@@ -3,6 +3,6 @@ cls
 ipconfig
 hostname
 timedate.cpl
-start \\%ip%\c$\larjr\img
-\\%10.102.18.98%\c$\larjr\exe\LightscreenPortable\LightscreenPortable.exe
+start \\%IPv4%\c$\larjr\img
+\\%IPv4%\c$\larjr\exe\LightscreenPortable\LightscreenPortable.exe
 shutdown -s -t 300 -f
